32e0fa65a91d9ab23a07db9357ef882d825884f6
Renaud Bechade <renaud.bechade@m4x.org>
2020-07-13 10:51:29 +0900

    windows

--- build-and-test.bat
+++ build-and-test.bat
@@ -4,7 +4,8 @@ cd build_release_windows
 del /F/Q/S *.*
 cmake -DUSE_CUDA=OFF -DUSE_JULIA=OFF -DCMAKE_BUILD_TYPE=Release ..
 if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild -m:6 -p:Configuration=Release Thetis.sln
+REM msbuild -m:6 -p:Configuration=Release Thetis.sln
+cmake --build .
 if %errorlevel% neq 0 exit /b %errorlevel%
 copy Release\*.exe .
 copy Release\*.dll .
